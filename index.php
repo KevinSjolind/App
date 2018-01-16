b8b1ee40414b07ba62a9353e38e4313925708163
Kevin Sjölind <kevin.sjolind@optistud.fi>
2018-01-16 12:29:24 +0200

    Added logout function

--- index.php
+++ index.php
@@ -4,7 +4,12 @@
   include "includes/head.php";
  ?>
 
+<?php if ($_SESSION['username']) : ?>
     <h1 class="animated zoomIn welcome">Välkommen, <?php echo $_SESSION['username']; ?></h1>
+    <a href="logout.php">Logga ut <?php echo $_SESSION['username']; ?></a>
+  <?php else : ?>
+    <h1>FA BORT!</h1>
+<?php endif; ?>
 
   </body>
 </html>
