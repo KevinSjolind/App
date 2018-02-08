aa2b2399177992a87c87917c4305cc296867d10d
Kevin Sjölind <kevin.sjolind@optistud.fi>
2018-02-08 13:56:51 +0200

    New splashscreen video

--- js/splash-screen.js
+++ js/splash-screen.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-//if (sessionStorage.getItem('splashScreen') !== 'true') {
+if (sessionStorage.getItem('splashScreen') !== 'true') {
   var quote = "What can you do the man is a god";
   var author = "EemeliTV";
 
@@ -25,7 +25,7 @@ $(document).ready(function() {
   $('blockquote').append('<footer id="author"></footer>');
   $('#quote').html(randomQuote.quote);
   $('#author').html(randomQuote.author);
-//  $("#splashScreen").show().delay(2500).fadeOut();
+  $("#splashScreen").show().delay(2500).fadeOut();
   sessionStorage.setItem('splashScreen', 'true');
-//}
+}
 });
